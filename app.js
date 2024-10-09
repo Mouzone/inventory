72bd67e85ef995c46e9deff0fab4cfb85fc11ccf
Mouzone <sunnyliu010@gmail.com>
2024-10-09 14:59:53 -0400

    Create basic starter files to template the path and install dotenv

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const path = require("node:path")
+const inventoryRouter = require("routes/inventoryRouter")
 const express = require("express")
-const {urlencoded} = require("express");
 const app = express()
 
 app.set("view", path.join(__dirname, "views"))
 app.set("view_engine", "ejs")
-app.use(urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
+app.use("/", inventoryRouter)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`Listening on Port ${PORT}`))
