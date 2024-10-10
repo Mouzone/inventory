752ef7ebdc2f97fb69c56be7fd7a5a5193402494
Mouzone <sunnyliu010@gmail.com>
2024-10-10 11:46:41 -0400

    Remove all references or functions related to soccer

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,37 +1,11 @@
 const db = require("../db/queries.js")
 
 module.exports.inventoryIndexGet = async (req, res) => {
-    const nationalities = await db.getAllNationalities()
-    const leagues = await db.getAllLeagues()
-    const teams = await db.getAllTeams()
-    const positions = await db.getAllPositions()
-    const players = await db.getAllPlayers()
-    res.render("index",
-        {
-            title: "Player-dex",
-            nationalities: nationalities.map((row) => row.nationality),
-            leagues: leagues.map(row => row.league),
-            teams: teams.map(row => row.team),
-            positions: positions.map(row => row.position),
-            players: players.map(row=> row.name),
-        })
+
 }
 
 module.exports.inventorySearchGet = async (req, res) => {
-    const nationalities = await db.getAllNationalities()
-    const leagues = await db.getAllLeagues()
-    const teams = await db.getAllTeams()
-    const positions = await db.getAllPositions()
-    const players = await db.search(req.params.name, req.params.nationality, req.params.team, req.params.league, req.params.position)
-    res.render("index",
-        {
-            title: "Player-dex",
-            nationalities: nationalities.map((row) => row.nationality),
-            leagues: leagues.map(row => row.league),
-            teams: teams.map(row => row.team),
-            positions: positions.map(row => row.position),
-            players: players.map(row=> row.name),
-        })
+
 }
 
 module.exports.inventoryNewGet = (req, res) => {
@@ -39,7 +13,5 @@ module.exports.inventoryNewGet = (req, res) => {
 }
 
 module.exports.inventoryNewPost = async (req, res) => {
-    await db.insert(req.body.nationality, req.body.birthdate, req.body.team, req.body.league, req.body.nationality,
-        req.body.position, req.body.start_yr, req.body.end_yr)
     res.redirect("/")
 }
