8309ed04a1f1ed6165567aec36e8b6d8b6f7630d
Mouzone <sunnyliu010@gmail.com>
2024-10-11 12:31:54 -0400

    Refactor routes to make it more readable based on pages they interact with

--- routes/inventoryRouter.js
+++ routes/inventoryRouter.js
@@ -6,13 +6,21 @@ inventoryRouter.get("/", inventoryController.inventoryIndexGet)
 inventoryRouter.get("/search", inventoryController.inventorySearchGet)
 inventoryRouter.get("/new", inventoryController.inventoryNewGet)
 inventoryRouter.post("/new", inventoryController.inventoryNewPost)
+inventoryRouter.get("/category/:category_id", inventoryController.inventoryCategoryGet)
+
+// todo: make another router for info
 inventoryRouter.get("/info/:emoji_id", inventoryController.inventoryInfoGet)
 inventoryRouter.post("/info/:emoji_id", inventoryController.inventoryInfoPost)
-inventoryRouter.get("/category/:category_id", inventoryController.inventoryCategoryGet)
-inventoryRouter.post("/edit/:emoji_id", inventoryController.inventoryEditPost)
+inventoryRouter.post("/info/:emoji_id/edit/name", inventoryController.inventoryEditPost)
+inventoryRouter.post("/info/:emoji_id/edit/category/", inventoryController.inventoryEditPost)
 
-// todo: add a filter to each category_list page to show more specific intersections
+
+
 // todo: figure out how to remove categories for a specific emoji
+// todo: add a filter to each category_list page to show more specific intersections
+// probably checkboxes on the side to tick off and apply them with a submit button
 // todo: figure out how to delete all categories for all emojis
 // todo: add sort functionality by date added
+// todo: add autocomplete suggestions when adding new categories b
+//  ased on categories that already exist
 module.exports = inventoryRouter
