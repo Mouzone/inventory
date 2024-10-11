8f6c4f4b009e389809fa2f351e39e4813d7b7d58
Mouzone <sunnyliu010@gmail.com>
2024-10-11 19:55:28 -0400

    Do not return result from controllers function if the result contains null returning instead empty array to avoid spawning remove button

--- controllers/emojiController.js
+++ controllers/emojiController.js
@@ -4,6 +4,9 @@ module.exports.emojiGet = async (req, res) => {
     const emojiSearchResult = await db.getEmojiByEmojiID(req.params.emoji_id)
     const emoji = emojiSearchResult[0]
     const categories = await db.getCategoriesByEmojiID(req.params.emoji_id)
+    if (!categories[0].category_name) {
+        categories.length = 0
+    }
     res.render("emoji", {title: "Info", emoji, categories})
 }
 
--- routes/emojiRouter.js
+++ routes/emojiRouter.js
@@ -5,6 +5,7 @@ const editController = require("../controllers/editController");
 
 emojiRouter.get("/:emoji_id", emojiController.emojiGet)
 emojiRouter.post("/:emoji_id/delete", emojiController.emojiDeletePost)
+
 emojiRouter.post("/:emoji_id/edit/name", editController.editNamePost)
 emojiRouter.post("/:emoji_id/edit/category/add", editController.editCategoryAddPost)
 emojiRouter.post("/:emoji_id/edit/category/delete/:category_id", editController.editCategoryDeletePost)
